d88faee426558b4f9f84ccbcaa7fbf744f7c122d
root <root@ip-172-31-41-3.ap-south-1.compute.internal>
2025-04-26 13:51:08 +0000

    added lambda function

---
+++ terraform/lambda_function/lambda_function.py
@@ -0,0 +1,24 @@
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    """
+    function to trigger on S3 object creation
+    and logs the event details to CloudWatch Logs, Ohh Yeah!
+    """
+    logger.info("Received event: %s", json.dumps(event, indent=2))
+
+
+    for record in event.get('Records', []):
+        bucket_name = record['s3']['bucket']['name']
+        object_key = record['s3']['object']['key']
+        logger.info(f"New object created in bucket: {bucket_name}, Object key: {object_key}")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('S3 Event logged successfully!')
+    }
+
